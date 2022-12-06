9b34431a5dc334b2653a809aec90d894e62cbf67
Chase Lukasiewicz <chaseluka@gmail.com>
2022-12-06 16:39:47 -0700

    webpack error fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(sass|less|css)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
       },
     ],
   },
